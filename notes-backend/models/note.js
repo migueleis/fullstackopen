3272d34e320f3a8552a37594b13ec9e89b1a263f
migueleis <migueleis@hotmail.com>
2023-10-27 07:26:19 +0200

    refactor

--- notes-backend/models/note.js
+++ notes-backend/models/note.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI;
-
-console.log('connecting to', url);
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  });
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
@@ -32,6 +20,5 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 });
-
 
 module.exports = mongoose.model('Note', noteSchema);
